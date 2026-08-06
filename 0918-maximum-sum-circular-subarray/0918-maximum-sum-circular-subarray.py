0dbab39a745fa9016b304ee7a9346262a2c76d8c
Eka Gunawan <56123064+radengunawan@users.noreply.github.com>
2026-08-06 20:43:43 +0700

    Time: 47 ms (41.18%), Space: 24.1 MB (61.8%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        globMax, globMin = nums[0], nums[0]
+        curMax, curMin = 0, 0
+        total = 0
+
+        for num in nums:
+            curMax = max(curMax + num, num)
+            curMin = min(curMin + num, num)
+            total += num
+            globMax = max(globMax, curMax)
+            globMin = min(globMin, curMin)
+
+        return max(globMax, total - globMin) if globMax > 0 else globMax
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
