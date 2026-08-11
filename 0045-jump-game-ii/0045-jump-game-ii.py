2690f49678fad705e946f39d7584dbdaf4e0cfeb
Eka Gunawan <56123064+radengunawan@users.noreply.github.com>
2026-08-11 08:22:18 +0700

    Time: 7 ms (50.63%), Space: 20.2 MB (33.66%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        res = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
